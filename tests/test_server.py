b33bcda05eaf72118f663fdcfc498e865a3f95c0
bibi21000 <bibi21000@gmail.com>
2016-02-21 17:13:42 +0100

    Important update on server reload and way of running. Add and update tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -63,13 +63,6 @@ class TestPiSerser(JNTTServer, JNTTServerCommon):
     server_conf = "tests/data/janitoo_raspberry_gpio.conf"
     hadds = [HADD%(145,0), HADD%(145,1), HADD%(145,2)]
 
-    def test_101_server_start_no_error_in_log(self):
+    def test_040_server_start_no_error_in_log(self):
         self.onlyRasperryTest()
-        self.start()
-        try:
-            time.sleep(120)
-        finally:
-            self.stop()
-        self.assertNotInLogfile('^ERROR ')
-        self.assertInLogfile('Found heartbeats in timeout')
-
+        JNTTServer.test_040_server_start_no_error_in_log(self)
